2d207c63fbd40527398b8d3d4b77317568a75272
Maria M <prov21_1@hotmail.com>
2021-06-28 20:25:36 -0700

    yelp api not working

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,58 @@ import {
   useQuery,
   gql,
 } from "@apollo/client";
+const getHeaders = require("./auth");
+const createHttpLink = require("apollo-link-http").createHttpLink;
+const setContext = require("apollo-link-context").setContext;
 
-const client = new ApolloClient({
+/* const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+}); */
+
+const httpLink = createHttpLink({
+  uri: "https://api.yelp.com/v3/graphql",
+  fetch: fetch,
+  //credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const authHeaders = getHeaders();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: authHeaders,
+  };
+});
+
+//set up client
+//concat http link with apollo auth
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-const EXCHANGE_RATES = gql`
+/* const EXCHANGE_RATES = gql`
   query GetExchangeRates {
     rates(currency: "USD") {
       currency
       rate
     }
   }
+`; */
+
+const RESTAURANT_SEARCH = gql`
+  {
+    business(id: "garaje-san-francisco") {
+      name
+      id
+      alias
+      rating
+      url
+    }
+  }
 `;
-function ExchangeRates() {
+
+/* function ExchangeRates() {
   const { loading, error, data } = useQuery(EXCHANGE_RATES);
 
   if (loading) return <p>Loading...</p>;
@@ -34,6 +71,22 @@ function ExchangeRates() {
       </p>
     </div>
   ));
+} */
+
+function RestaurantSearch() {
+  console.log("hello yelp");
+  const { loading, data, error } = useQuery(RESTAURANT_SEARCH);
+  const restaurantData = data?.business || {};
+  console.log(restaurantData);
+  if (loading) return <p>Loading...</p>;
+  //if (error) return <p>Error :(</p>;
+  console.log(error);
+
+  return (
+    <div key={restaurantData.name}>
+      <p>{restaurantData.name}</p>
+    </div>
+  );
 }
 
 function App() {
@@ -43,7 +96,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Welcome to TasteBuds.</p>
-          <ExchangeRates />
+          <RestaurantSearch />
           <a
             className="App-link"
             href="https://reactjs.org"
