2b7110836f276e3f1db7848eef854242eba842cb
George <gms1980@icloud.com>
2021-06-30 07:59:27 -0600

    updated searchRestaurant.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 //const db = require("./config/connection");
 //const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
-//const { authMiddleware } = require("./utils/auth");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
