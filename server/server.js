abf7bdfae8a954fd73a907fd40bb5a5f4ccdb6a6
George <gms1980@icloud.com>
2021-07-01 08:48:11 -0700

    updated folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-//const db = require("./config/connection");
-//const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 
