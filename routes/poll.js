6c211878045406d4f8a2483769c76caadf18c314
Maria M <prov21_1@hotmail.com>
2021-07-02 09:41:54 -0700

    pusher

---
+++ routes/poll.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+//
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1228744",
+  key: "3cb02dbd0c542bff3bd5",
+  secret: "18a0f8a7e8854f574cc5",
+  cluster: "us3",
+  useTLS: true,
+});
+
+//
+router.get("/", (req, res) => {
+  //fetch and send polls
+  res.send("POLL");
+});
+
+router.post("/", (req, res) => {
+  pusher.trigger("tastebuds", "tastebudvote", {
+    points: 1,
+    restaurant: req.body.restaurant,
+  });
+  return res.json({ success: true, message: "Thank you vor voting!" });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 
+//FOR POLL
+const bodyParser = require("body-parser");
+const cors = require("cors");
+//body parser middleware for poll
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+const poll = require("../routes/poll");
+app.use("../routes/poll", poll);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -14,32 +24,26 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 //if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-  
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-  
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-  
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      //log where we can go to test our GQL API // to see built in apolo test npm run watch
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    //log where we can go to test our GQL API // to see built in apolo test npm run watch
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-  
-  module.exports=db;
-  
+});
+
+module.exports = db;
