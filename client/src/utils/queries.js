9966a29207bdb568cac9e410252fe7d662931fbc
Maria M <prov21_1@hotmail.com>
2021-07-03 13:38:21 -0700

    merge and client server starts

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import { gql } from '@apollo/client';
-
-export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
-      _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
-        _id
-      }
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
-      }
-=======
 import { gql } from "@apollo/client";
 
 export const QUERY_RESTAURANTS = gql`
@@ -73,7 +12,6 @@ export const QUERY_RESTAURANTS = gql`
       location
       phone
       image_url
->>>>>>> b6c2a3da8e4822274bf5cbe5bad5415212a4aaac
     }
   }
 `;
