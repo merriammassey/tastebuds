ddbcbb6cfed052bd7000f55e860d0b25951f960a
Maria M <prov21_1@hotmail.com>
2021-07-04 10:32:41 -0700

    updated typedefs and models

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,37 +1,38 @@
-const { Schema, model } = require('mongoose');
-const restaurantSchema = require("./Restaurant")
-const dateFormat = require('../utils/dateFormat');
-const userSchema = require ("./User")
+const { Schema, model } = require("mongoose");
+const restaurantSchema = require("./Restaurant");
+const dateFormat = require("../utils/dateFormat");
+const userSchema = require("./User");
 
-const eventSchema = new Schema (
-    {
-        eventName: {
-            type: String,
-            required: 'You need to name your event',
-            minlength: 1,
-            maxlength: 30
-        },
-        createAt: {
-            type: Date, 
-            default: Date.now,
-            get: timestamp => dateFormat(timestamp)
-        },
-        username: {
-            type: String, 
-            required: true
-        },
-        invites: [userSchema],
-        restaurant: [restaurantSchema]
+const eventSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: "You need to name your event",
+      minlength: 1,
+      maxlength: 30,
     },
-    {
-        toJSON: {
-          getters: true
-        }
-      }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+    },
+    //invites: [userSchema],
+    restaurants: [restaurantSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
 );
 
+const Event = model("Event", eventSchema);
 
-const Event = model ('Event', eventSchema);
-
-module.exports = Event; 
-
+module.exports = Event;
--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,37 +1,41 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const restaurantSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    categories: {
-        type: String
-    },
-    url: {
-        type: String
-    },
-    rating: {
-        type: String
-    },
-    price: {
-        type: String
-    },
-    location: {
-        type: String
-    },
-    phone: {
-        type: String
-    },
-    image: {
-        type: String
-    }
+  restaurantId: {
+    type: String,
+    required: true,
+  },
+  key: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  rating: {
+    type: String,
+  },
+  address1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  image_url: {
+    type: String,
+  },
 });
 
+// creating model
+const Restaurant = model("Restaurant", restaurantSchema);
 
-
-
-// creating model 
-const Restaurant = model('Restaurant', restaurantSchema);
-
-module.export = Restaurant
+module.export = Restaurant;
