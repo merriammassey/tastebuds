d640c20971e16ce81298c0d82ebdfa1fd7d94479
Maria M <prov21_1@hotmail.com>
2021-08-12 17:18:10 -0700

    deploy

--- client/config.js
+++
@@ -1,6 +0,0 @@
-// Replace these values
-const YELP_API_KEY =
-  "LhPwa5pQMDo4DVNiUSePZ5L0Ge_Qof4n3cXJNWdfW1kw0-O6PzbebTK78f-nuEcNACakrZSDWLYYBxi-gAj7Yk7zSazSRnIXn7QOtV0KEkV7ca9F2djgWWzF-gjWYHYx";
-const YELP_API_SECRET = "sshhsecret";
-
-module.exports = { YELP_API_KEY, YELP_API_SECRET };
