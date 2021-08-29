a9f617e5145298a285fe274bde19f8db7fdf956d
Maria M <prov21_1@hotmail.com>
2021-08-29 16:48:24 -0700

    auto close modal after alert

--- client/src/components/ShareIcons2.js
+++ client/src/components/ShareIcons2.js
@@ -22,8 +22,8 @@ const ShareIcons = ({ eventid }) => {
   const props = { eventid };
   console.log(eventid);
   const title = "Please complete my TasteBuds poll";
-  //const shareUrl = `https://whereyouwannaeat.herokuapp.com/vote/${eventid}`;
-  console.log(shareUrl);
+  const shareUrl = `https://whereyouwannaeat.herokuapp.com/vote/${eventid}`;
+  //console.log(shareUrl);
   /*  const shareUrl = () => {
     if (window.location.toString().includes("viewevent")) {
       const shareUrl = `https://www.whereyouwannaeat.herokuapp.com/vote/${eventid}`;
